d3bd48413d22bae813dafde9ce57286b6139b2bb
Edward Tanguay (windows) <edward@tanguay.info>
2023-02-07 11:48:40 +0100

    works

--- src/server.ts
+++ src/server.ts
@@ -9,34 +9,34 @@ app.use(cors());
 const httpServer = http.createServer(app);
 
 const io = new Server(httpServer, {
-    cors: {
-        origin: '*',
-        methods: ["GET", "POST"],
-    },
+	cors: {
+		origin: '*',
+		methods: ["GET", "POST"],
+	},
 });
 
 app.get('/', (req: express.Request, res: express.Response) => {
-    res.send(`
-<h1>Socket.io Server</h1>
+	res.send(`
+<h1>Socket.io Server</h1>	
 <p>This backend connects socket.io clients and enables communication between them.</p>
-    `)
+	`)
 });
 
 io.on("connection", (socket) => {
-    console.log(`User Connected: ${socket.id}`);
- 
-    socket.on("join_room", (data) => {
-        socket.join(data);
-        console.log(`User with ID: ${socket.id} joined room: ${data}`);
-    });
- 
-    socket.on("send_message", (data) => {
-        socket.to(data.room).emit("receive_message", data);
-    });
- 
-    socket.on("disconnect", () => {
-        console.log("User Disconnected", socket.id);
-    });
+	console.log(`User Connected: ${socket.id}`);
+
+	socket.on("join_room", (data) => {
+		socket.join(data);
+		console.log(`User with ID: ${socket.id} joined room: ${data}`);
+	});
+
+	socket.on("send_message", (data) => {
+		socket.to(data.room).emit("receive_message", data);
+	});
+
+	socket.on("disconnect", () => {
+		console.log("User Disconnected", socket.id);
+	});
 });
 
 httpServer.listen(config.port, () => {
