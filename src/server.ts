6beb42bb6953feaa30b9f8e36c87aa02705f9b2b
Edward Tanguay (windows) <edward@tanguay.info>
2023-02-07 11:03:28 +0100

    finished

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,44 @@
 import express from 'express';
+import http from 'http';
 import cors from 'cors';
 import * as config from './config.js';
+import { Server } from 'socket.io';
 
 const app = express();
 app.use(cors());
+const httpServer = http.createServer(app);
 
-app.get('/', (req: express.Request, res: express.Response) => {
-	res.send('socket.io demo')
+const io = new Server(httpServer, {
+    cors: {
+        origin: '*',
+        methods: ["GET", "POST"],
+    },
 });
 
-app.listen(config.port, () => {
+app.get('/', (req: express.Request, res: express.Response) => {
+    res.send(`
+<h1>Socket.io Server</h1>
+<p>This backend connects socket.io clients and enables communication between them.</p>
+    `)
+});
+
+io.on("connection", (socket) => {
+    console.log(`User Connected: ${socket.id}`);
+ 
+    socket.on("join_room", (data) => {
+        socket.join(data);
+        console.log(`User with ID: ${socket.id} joined room: ${data}`);
+    });
+ 
+    socket.on("send_message", (data) => {
+        socket.to(data.room).emit("receive_message", data);
+    });
+ 
+    socket.on("disconnect", () => {
+        console.log("User Disconnected", socket.id);
+    });
+});
+
+httpServer.listen(config.port, () => {
 	console.log(`listening on port http://localhost:${config.port}`);
 });
